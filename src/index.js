cb6dbdf69c031a117973e279fb3a5af5fe9e68e8
KhailoAndrey <hap512@gmail.com>
2023-05-14 13:15:02 +0300

    start

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import { App } from 'components/App';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { store,persistor } from 'redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const theme = {
   colors: {
@@ -17,8 +19,15 @@ const theme = {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <PersistGate loading={null} persistor={persistor}>
+          
+
+        <BrowserRouter basename='/goit-react-hw-08-phonebook'>
+
       <App />
+        </BrowserRouter>
+              </PersistGate>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>
