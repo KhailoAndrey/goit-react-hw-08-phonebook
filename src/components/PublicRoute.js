660bb535a2ab481300a1cbf741adddef67b23e07
Andrey Khaylo <110947394+KhailoAndrey@users.noreply.github.com>
2023-05-15 14:38:27 +0300

    Update PublicRoute.js

--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -5,7 +5,7 @@ import { Navigate } from 'react-router-dom';
 const PublicRoute = ({ children }) => {
   const isAuth = useSelector(state => state.auth.isLoggedIn)
   
-  return !isAuth ? children : <Navigate to="/" /> ;
+  return isAuth ? children : <Navigate to="/" /> ;
 };
 
 export default PublicRoute;
